8188f413787cb4280111dbfffe50dcb9e17dff24
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-03-05 20:17:39 +0200

    Adaugare obiect

--- src/Map/HashMapEx1.java
+++ src/Map/HashMapEx1.java
@@ -9,6 +9,7 @@ public class HashMapEx1 {
         map1.put(125, "Popescu Valeriu");
         map1.put(128, "Frimu Victor");
         map1.put(132, "Prisacaru Anatol");
+        map1.put(139, "Prisacaru Gicu");
         System.out.println(map1);
 //        System.out.println(map1.get(125));
 //        System.out.println(map1.containsValue("Prisacaru Anatol"));
