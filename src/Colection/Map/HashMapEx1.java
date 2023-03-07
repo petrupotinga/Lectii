47643dd6d6a11dcf5ce852d620d0e0efb6b67f13
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-03-07 19:12:23 +0200

    Update HashMapEx1.java

--- src/Colection/Map/HashMapEx1.java
+++ src/Colection/Map/HashMapEx1.java
@@ -5,15 +5,15 @@ import java.util.Map;
 
 public class HashMapEx1 {
     public static void main(String[] args) {
-        Map<Integer,String> map1 = new HashMap<>();
+        Map<Integer, String> map1 = new HashMap<>();
         map1.put(125, "Popescu Valeriu");
         map1.put(128, "Frimu Victor");
         map1.put(132, "Prisacaru Anatol");
         map1.put(139, "Prisacaru Gicu");
         System.out.println(map1);
-//        System.out.println(map1.get(125));
-//        System.out.println(map1.containsValue("Prisacaru Anatol"));
-//        System.out.println(map1.containsKey(127));
+        System.out.println(map1.get(125));
+        System.out.println(map1.containsValue("Prisacaru Anatol"));
+        System.out.println(map1.containsKey(127));
         map1.remove(128);
         System.out.println(map1);
         System.out.println(map1.keySet());
