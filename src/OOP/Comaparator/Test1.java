ea049e6371d66e9a9b5354d6be51ddf5e4ea0c87
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-03-13 11:23:17 +0200

    Update Test1.java

--- src/OOP/Comaparator/Test1.java
+++ src/OOP/Comaparator/Test1.java
@@ -1,29 +1,26 @@
 package OOP.Comaparator;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 public class Test1 {
     public static void main(String[] args) {
-        List<Employee> list = new ArrayList<Employee>();
-        Employee emp1 = new Employee(100, "Zaur", "Tregulov", 1255);
-        Employee emp2 = new Employee(125, "Ion", "Prisacaru ", 1255);
-        Employee emp3 = new Employee(108, "Anatol", "Croitor", 1255);
+        List<Employee1> list = new ArrayList<Employee1>();
+        Employee1 emp1 = new Employee1(100, "Zaur", "Tregulov", 1255);
+        Employee1 emp2 = new Employee1(125, "Ion", "Prisacaru ", 1255);
+        Employee1 emp3 = new Employee1(108, "Anatol", "Croitor", 1255);
         list.add(emp1);
         list.add(emp2);
         list.add(emp3);
-        Collections.sort(list, new IdComarator());
+//        Collections.sort(list, new IdComarator());
         System.out.println(list);
     }
 }
-
-class Employee {
-    int id;
-    String name;
-    String surname;
-    int salary;
+ class Employee {
+    private int id;
+    private String name;
+    private String surname;
+    private int salary;
 
     public Employee(int id, String name, String surname, int salary) {
         this.id = id;
@@ -42,21 +39,21 @@ class Employee {
                 '}';
     }
 }
+//
+//class IdComarator implements Comparator<Employee1> {
+//
+//    @Override
+//    public int compare(Employee1 emp1, Employee1 emp2) {
+//        if (emp1.id == emp2.id) {
+//            return 0;
+//        } else if (emp1.id < emp2.id) {
+//            return -1;
+//        } else {
+//            return 1;
+//
+//        }
+//    }
 
-class IdComarator implements Comparator<Employee> {
-
-    @Override
-    public int compare(Employee emp1, Employee emp2) {
-        if (emp1.id == emp2.id) {
-            return 0;
-        } else if (emp1.id < emp2.id) {
-            return -1;
-        } else {
-            return 1;
-
-        }
-    }
-}
 //class NameComparator implements Comparator <Employee>{
 //
 //    @Override
