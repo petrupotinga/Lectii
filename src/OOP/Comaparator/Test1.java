a92747b6ebe348f9cf32689a602275ca669a48a7
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-03-14 19:38:03 +0200

    Update Test1.java

--- src/OOP/Comaparator/Test1.java
+++ src/OOP/Comaparator/Test1.java
@@ -1,26 +1,31 @@
 package OOP.Comaparator;
 
+import OOP.Comaparator.Employee.NameComparator;
+
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Test1 {
     public static void main(String[] args) {
-        List<Employee1> list = new ArrayList<Employee1>();
-        Employee1 emp1 = new Employee1(100, "Zaur", "Tregulov", 1255);
-        Employee1 emp2 = new Employee1(125, "Ion", "Prisacaru ", 1255);
-        Employee1 emp3 = new Employee1(108, "Anatol", "Croitor", 1255);
+        List<Employee> list = new ArrayList<Employee>();
+        Employee emp1 = new Employee(100, "Zaur", "Tregulov", 12358);
+        Employee emp2 = new Employee(125, "Ion", "Prisacaru ", 14555);
+        Employee emp3 = new Employee(108, "Anatol", "Croitor", 18555);
         list.add(emp1);
         list.add(emp2);
         list.add(emp3);
-//        Collections.sort(list, new IdComarator());
+        Collections.sort(list, new NameComparator());
         System.out.println(list);
     }
 }
- class Employee {
-    private int id;
-    private String name;
-    private String surname;
-    private int salary;
+
+class Employee implements Comparable<Employee> {
+    int id;
+    String name;
+    String surname;
+    int salary;
 
     public Employee(int id, String name, String surname, int salary) {
         this.id = id;
@@ -38,26 +43,46 @@ public class Test1 {
                 ", salary=" + salary +
                 '}';
     }
-}
-//
-//class IdComarator implements Comparator<Employee1> {
-//
-//    @Override
-//    public int compare(Employee1 emp1, Employee1 emp2) {
-//        if (emp1.id == emp2.id) {
-//            return 0;
-//        } else if (emp1.id < emp2.id) {
-//            return -1;
-//        } else {
-//            return 1;
-//
-//        }
-//    }
 
-//class NameComparator implements Comparator <Employee>{
-//
-//    @Override
-//    public int compare(Employee emp1, Employee emp2) {
-//        return emp1.name.compareTo(emp2.name);
-//    }
-//}
+    @Override
+    public int compareTo(Employee o) {
+        if (this.id == o.id) {
+            return 0;
+        } else if (this.id < o.id) {
+            return -1;
+        } else {
+            return 1;
+        }
+    }
+
+
+    static class IdComarator implements Comparator<Employee> {
+
+        @Override
+        public int compare(Employee emp1, Employee emp2) {
+            if (emp1.id == emp2.id) {
+                return 0;
+            } else if (emp1.id < emp2.id) {
+                return -1;
+            } else {
+                return 1;
+
+            }
+        }
+    }
+
+    static class NameComparator implements Comparator<Employee> {
+
+        @Override
+        public int compare(Employee emp1, Employee emp2) {
+            return emp1.name.compareTo(emp2.name);
+        }
+    }
+
+    static class SalaryComarator implements Comparator<Employee> {
+        @Override
+        public int compare(Employee emp1, Employee emp2) {
+            return emp1.salary - emp2.salary;
+        }
+    }
+}
