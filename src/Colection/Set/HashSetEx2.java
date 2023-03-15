9fd5a4a7d1d4c6250e880813edcdb41f5697bfca
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-03-15 17:09:45 +0200

    Create HashSetEx2.java

---
+++ src/Colection/Set/HashSetEx2.java
@@ -0,0 +1,26 @@
+package Colection.Set;
+
+import java.util.HashSet;
+
+public class HashSetEx2 {
+    public static void main(String[] args) {
+        HashSet<Integer> hashSet1 = new HashSet<>();
+        hashSet1.add(5);
+        hashSet1.add(2);
+        hashSet1.add(3);
+        hashSet1.add(1);
+        hashSet1.add(8);
+
+        HashSet<Integer> hashSet2 = new HashSet<>();
+        hashSet2.add(7);
+        hashSet2.add(4);
+        hashSet2.add(3);
+        hashSet2.add(5);
+        hashSet2.add(8);
+
+        HashSet<Integer> union = new HashSet<>(hashSet1);
+        union.addAll(hashSet2);
+        System.out.println(union);
+
+    }
+}
