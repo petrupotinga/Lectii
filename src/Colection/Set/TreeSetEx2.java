a179b608ee9ddfaa5fbb8948f6dde6464c5d9832
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-03-16 21:58:54 +0200

    Create TreeSetEx2.java

---
+++ src/Colection/Set/TreeSetEx2.java
@@ -0,0 +1,43 @@
+package Colection.Set;
+
+import java.util.TreeSet;
+
+public class TreeSetEx2 {
+    public static void main(String[] args) {
+        TreeSet<Student> treeSet = new TreeSet<>();
+        Student st1 = new Student("Mihai", 2);
+        Student st2 = new Student("Constantin", 4);
+        Student st3 = new Student("Renat", 3);
+        Student st4 = new Student("Ion", 5);
+        Student st5 = new Student("Vitalii", 1);
+        treeSet.add(st1);
+        treeSet.add(st2);
+        treeSet.add(st3);
+        treeSet.add(st4);
+        treeSet.add(st5);
+        System.out.println(treeSet);
+    }
+}
+
+class Student implements Comparable<Student> {
+    String name;
+    int course;
+
+    public Student(String name, int course) {
+        this.name = name;
+        this.course = course;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", course=" + course +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Student other) {
+        return this.course - other.course;
+    }
+}
