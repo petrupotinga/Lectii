a588455359356c7e15f80a0644bac37e53c30f7c
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-03-16 21:29:41 +0200

    Create TreeSetEx1.java

---
+++ src/Colection/Set/TreeSetEx1.java
@@ -0,0 +1,19 @@
+package Colection.Set;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+public class TreeSetEx1 {
+    public static void main(String[] args) {
+        Set<Integer> treeSet = new TreeSet<>();
+        treeSet.add(5);
+        treeSet.add(8);
+        treeSet.add(2);
+        treeSet.add(1);
+        treeSet.add(10);
+        System.out.println(treeSet);
+        treeSet.remove(2);
+        System.out.println(treeSet);
+        System.out.println(treeSet.contains(2));
+    }
+}
