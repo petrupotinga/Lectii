adf6652ac2c33b5fd2ff408785c30d65db7814fe
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-03-23 21:01:19 +0200

    Update TreeMapEx.java

--- src/Colection/Map/TreeMap/TreeMapEx.java
+++ src/Colection/Map/TreeMap/TreeMapEx.java
@@ -31,9 +31,9 @@ public class TreeMapEx {
 }
 
 class Student {    // implements Comparable<Student> {
-    String name;
-    String surname;
-    int course;
+    private String name;
+    private String surname;
+    private int course;
 
     public Student(String name, String surname, int course) {
         this.name = name;
