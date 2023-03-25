72e629e931ef9222aceea7fc43808a1d084566b9
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-03-25 18:04:14 +0200

    Update ScanerEx1.java

--- src/Scan/ScanerEx1.java
+++ src/Scan/ScanerEx1.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class ScanerEx1 {
     public static void main(String[] args) {
-//        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 //        System.out.println("introdu numarul");
 //        int i = scanner.nextInt();
 //        System.out.println("Numarul introdus :" + i);
@@ -13,23 +13,23 @@ public class ScanerEx1 {
 //        int y = scanner.nextInt();
 //        System.out.println("Intrgul : " + x/y);
 //        System.out.println("Restul : " + x%y);
-//        System.out.println("scrieti doua rinduri:");
-//        String s1 = scanner.nextLine();
-//        String s2 = scanner.nextLine();
-//        System.out.println("Ati scris in primul rind : " + s1);
-//        System.out.println("Ati scris in al doilea rind : " + s2);
+        System.out.println("scrieti doua rinduri:");
+        String s1 = scanner.nextLine();
+        String s2 = scanner.nextLine();
+        System.out.println("Ati scris in primul rind : " + s1);
+        System.out.println("Ati scris in al doilea rind : " + s2);
 //        System.out.println("introdu numarul: ");
 //        double i = scanner.nextDouble();
 //        System.out.println("Numarul introdus :" + i);
-        Scanner scanner = new Scanner("Salut prietene! \n Cum dispozitia \n" +
-                "ce mai este nou? ");
+//        Scanner scanner = new Scanner("Salut prietene! \n Cum dispozitia \n" +
+//                "ce mai este nou? ");
 //        String s1 = scanner.nextLine();
 //        System.out.println(s1);
 //        System.out.println(scanner.nextLine());
 //        System.out.println(scanner.nextLine());
-        while (scanner.hasNextLine()) {
-            System.out.println(scanner.nextLine());
+//        while (scanner.hasNextLine()) {
+//            System.out.println(scanner.nextLine());
         }
 //        System.out.println(scanner.next().charAt(2));
     }
-}
+
