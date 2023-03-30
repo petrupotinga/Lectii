01fc7410a3398b6f1474be589cd9b74240c4a8a1
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-03-30 19:06:35 +0300

    Create ExercitiiGresite.java

---
+++ src/Scan/ExercitiiGresite.java
@@ -0,0 +1,22 @@
+package Scan;
+
+public class ExercitiiGresite {
+    int x;
+    int y;
+    int suma;
+
+    public ExercitiiGresite(int x, int y, int suma) {
+        this.x = x;
+        this.y = y;
+        this.suma = suma;
+    }
+
+    @Override
+    public String toString() {
+        return "ExercitiiGresite{" +
+                "x=" + x +
+                ", y=" + y +
+                ", suma=" + suma +
+                '}';
+    }
+}
