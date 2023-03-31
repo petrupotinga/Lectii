545da6ec40d7cf78fdf38c632692f2b8b1a729e3
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-03-31 19:31:40 +0300

    Create TestCar.java

---
+++ src/Nested/Static/TestCar.java
@@ -0,0 +1,8 @@
+package Nested.Static;
+
+public class TestCar {
+    public static void main(String[] args) {
+        Car.Engine engine = new Car.Engine(280);
+        System.out.println(engine);
+    }
+}
