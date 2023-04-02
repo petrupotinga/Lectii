4bb07a682998fb5c29658e42ec19e8e540582c34
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-04-02 22:34:02 +0300

    Update TestCar.java

--- src/Nested/Static/TestCar.java
+++ src/Nested/Static/TestCar.java
@@ -4,5 +4,14 @@ public class TestCar {
     public static void main(String[] args) {
         Car.Engine engine = new Car.Engine(280);
         System.out.println(engine);
+        Car car = new Car("red", 2, engine);
+        System.out.println(car);
+
+    }
+}
+
+class z extends Car.Engine {
+    z() {
+        super(200);
     }
 }
