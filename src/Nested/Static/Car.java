b40453774e678b0be026baee1bd42b8696a2cb5c
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-04-03 20:06:54 +0300

    Update Car.java

--- src/Nested/Static/Car.java
+++ src/Nested/Static/Car.java
@@ -2,29 +2,40 @@ package Nested.Static;
 
 public class Car {
     String color;
-    int doorcount;
+    int doorCount;
     Engine engine;
+    private static int a;
+
 
     public Car(String color, int doorcount, Engine engine) {
         this.color = color;
-        this.doorcount = doorcount;
+        this.doorCount = doorCount;
         this.engine = engine;
+    }
+    void method(){
+        System.out.println(Engine.countOfObjects);
+        Engine e = new Engine(200);
+        System.out.println(e.horsePower);
     }
 
     @Override
     public String toString() {
         return "My car{" +
                 "color='" + color + '\'' +
-                ", doorcount=" + doorcount +
+                ", doorcount=" + doorCount +
                 ", engine=" + engine +
                 '}';
     }
 
     public static class Engine {
         int horsePower;
+        static int countOfObjects;
 
         public Engine(int horsePower) {
+            System.out.println(a);
+//            System.out.println(doorCount);
             this.horsePower = horsePower;
+            countOfObjects ++;
         }
 
         @Override
