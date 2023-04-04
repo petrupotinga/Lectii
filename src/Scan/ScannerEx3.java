825ea51284c0bc043b162f1e8521a91d14f71027
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-04-04 14:30:43 +0300

    Update ScannerEx3.java

--- src/Scan/ScannerEx3.java
+++ src/Scan/ScannerEx3.java
@@ -4,41 +4,45 @@ import java.util.*;
 
 public class ScannerEx3 {
     public static void main(String[] args) {
-
+        final int NUMARUL_TOTAL_DE_EXERCITII = 3;
+        final int MAX = 10;
         Scanner scanner = new Scanner(System.in);
-        List<ExercitiiGresite> raspunsuri = new ArrayList<>();
+        List<Exercitiu> exercitiiGresite = new ArrayList<>();
         Random r = new Random();
-        for (int i = 1; i <= 3; i++) {
-            int x = r.nextInt(10) + 1;
-            int y = r.nextInt(10) + 1;
-            int suma = x + y;
+        for (int i = 0; i < NUMARUL_TOTAL_DE_EXERCITII; i++) {
+            int x = r.nextInt(MAX) + 1;
+            int y = r.nextInt(MAX) + 1;
+            final int suma = x + y;
             System.out.println(x);
             System.out.println(y);
-            ExercitiiGresite ex = new ExercitiiGresite(x, y, suma);
-            String exercitiu = x + " + " + y + " = ";
-            System.out.println(exercitiu);
+            Exercitiu exercitiuCurent = new Exercitiu(x, y, suma);
+            System.out.println(exercitiuCurent.showExercitiu());
             int raspuns = scanner.nextInt();
             System.out.println("raspunsul primit :" + raspuns);
             if (suma == raspuns) {
                 System.out.println("raspuns corect, Bravo!");
             } else {
-                System.out.println("raspuns gresit: ");
-                raspunsuri.add(ex);
+                System.out.println("raspuns gresit ");
+                exercitiiGresite.add(exercitiuCurent);
             }
         }
-        System.out.println(raspunsuri);
+//        System.out.println(exercitiiGresite);
 
-        while (!raspunsuri.isEmpty()) {
-            ExercitiiGresite e = raspunsuri.get(0);
-            System.out.println(e);
+        for (Exercitiu e : exercitiiGresite) {
+            System.out.print(e.showExercitiu() + "  ");
+
+        }
+
+        while (!exercitiiGresite.isEmpty()) {
+            Exercitiu primulExercitiu = exercitiiGresite.get(0);
+            System.out.println(primulExercitiu.showExercitiu());
             int raspuns = scanner.nextInt();
-            if (e.suma == raspuns) {
-                raspunsuri.remove(e);
+            if (primulExercitiu.suma == raspuns) {
+                exercitiiGresite.remove(primulExercitiu);
             }
         }
-        System.out.println(raspunsuri);
+        System.out.println(exercitiiGresite);
         scanner.close();
-
     }
 }
 
