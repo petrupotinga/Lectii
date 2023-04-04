b896030a3fee68510ff5d7f18719a50e0b6a16e0
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-04-04 13:51:43 +0300

    Update ScannerEx3.java

--- src/Scan/ScannerEx3.java
+++ src/Scan/ScannerEx3.java
@@ -1,12 +1,10 @@
 package Scan;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class ScannerEx3 {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
         List<ExercitiiGresite> raspunsuri = new ArrayList<>();
         Random r = new Random();
@@ -16,19 +14,32 @@ public class ScannerEx3 {
             int suma = x + y;
             System.out.println(x);
             System.out.println(y);
-            ExercitiiGresite ex = new ExercitiiGresite(x,y,suma);
+            ExercitiiGresite ex = new ExercitiiGresite(x, y, suma);
             String exercitiu = x + " + " + y + " = ";
             System.out.println(exercitiu);
             int raspuns = scanner.nextInt();
             System.out.println("raspunsul primit :" + raspuns);
             if (suma == raspuns) {
                 System.out.println("raspuns corect, Bravo!");
-            }  else {
+            } else {
                 System.out.println("raspuns gresit: ");
                 raspunsuri.add(ex);
             }
         }
-        scanner.close();
         System.out.println(raspunsuri);
+
+        while (!raspunsuri.isEmpty()) {
+            ExercitiiGresite e = raspunsuri.get(0);
+            System.out.println(e);
+            int raspuns = scanner.nextInt();
+            if (e.suma == raspuns) {
+                raspunsuri.remove(e);
+            }
+        }
+        System.out.println(raspunsuri);
+        scanner.close();
+
     }
 }
+
+
