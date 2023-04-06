38b88880fb5f77b848e5e8bde46abefbd118ea63
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-04-06 20:04:15 +0300

    Update Main.java

--- src/OOP/Modificatori/Main.java
+++ src/OOP/Modificatori/Main.java
@@ -2,10 +2,10 @@ package OOP.Modificatori;
 
 public class Main {
     public static void main(String[] args) {
-        Test man = new Test("Ion", 18);
-        man.setAge(24);
-        man.showInfo();
-        System.out.println(man.getAge());
-        System.out.println(man.getName());
+        Person person = new Person();
+        person.setAge(1);
+        person.setName("");
+        System.out.println(person.getAge());
+        System.out.println(person.getName());
     }
 }
