707d1a7041d6d775b7cd5d19f2d15815de89e2d8
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-04-06 20:06:00 +0300

    Update Main.java

--- src/OOP/Modificatori/Main.java
+++ src/OOP/Modificatori/Main.java
@@ -3,8 +3,8 @@ package OOP.Modificatori;
 public class Main {
     public static void main(String[] args) {
         Person person = new Person();
-        person.setAge(1);
-        person.setName("");
+        person.setAge(18);
+        person.setName("Ion");
         System.out.println(person.getAge());
         System.out.println(person.getName());
     }
