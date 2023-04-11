2648a66a75d06fe96cc3479db47c65cb04a6a341
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-04-11 17:48:30 +0300

    Update Animal.java

--- src/OOP/Principii/Mostenirea/Animal.java
+++ src/OOP/Principii/Mostenirea/Animal.java
@@ -17,3 +17,14 @@ class Dog extends Animal {
         System.out.println("Câinele spune: bow wow");
     }
 }
+
+class Polimorfism {
+    public static void main(String[] args) {
+        Animal myAnimal = new Animal();
+        Animal myPig = new Pig();
+        Animal myDog = new Dog();
+        myAnimal.animalSound();
+        myPig.animalSound();
+        myDog.animalSound();
+    }
+}
