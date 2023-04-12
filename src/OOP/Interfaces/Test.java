dafd4684033879bfdc9fc9f9f9f91b97235caa01
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-04-12 13:31:48 +0300

    Update Test.java

--- src/OOP/Interfaces/Test.java
+++ src/OOP/Interfaces/Test.java
@@ -4,7 +4,11 @@ public class Test {
     public static void main(String[] args) {
         Animal animal1 = new Animal(121);
         Person person1 = new Person("Vasile");
-        animal1.showInfo();
-        person1.showInfo();
+        outputInfo(animal1);
+        outputInfo(person1);
+    }
+
+    public static void outputInfo(Info info) {
+        info.showInfo();
     }
 }
