e2bfb776c45f957c135af433dc568b373adaf3ee
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-04-12 13:31:06 +0300

    Update Animal.java

--- src/OOP/Interfaces/Animal.java
+++ src/OOP/Interfaces/Animal.java
@@ -2,9 +2,15 @@ package OOP.Interfaces;
 
 public class Animal implements Info {
     private int id;
-    public Animal(int id){
+
+    public Animal(int id) {
         this.id = id;
     }
+
+    public void sleep() {
+        System.out.println("dorm");
+    }
+
     @Override
     public void showInfo() {
         System.out.println("Id este: " + id);
