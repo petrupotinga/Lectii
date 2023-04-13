69154c00fe66f29a33ce8e7906dfba07de7adc73
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-04-13 13:51:46 +0300

    Create Patrat.java

---
+++ src/OOP/Principii/Polimorfism/Patrat.java
@@ -0,0 +1,8 @@
+package OOP.Principii.Polimorfism;
+
+public class Patrat extends Figuri{
+    @Override
+    public void aria() {
+        System.out.println("Patratul : latura * latura");
+    }
+}
