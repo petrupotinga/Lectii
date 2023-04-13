0d5463fc3af895fdac7b59700136ca8ef4f872d8
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-04-13 13:52:04 +0300

    Create Triunghi.java

---
+++ src/OOP/Principii/Polimorfism/Triunghi.java
@@ -0,0 +1,8 @@
+package OOP.Principii.Polimorfism;
+
+public class Triunghi extends Figuri {
+    @Override
+    public void aria() {
+        System.out.println("Triunghiul:  ½ * latura * inaltime");
+    }
+}
