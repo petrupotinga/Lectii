f6a5858243128f235fd5b1f6ae13abbbb95192c8
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-04-18 13:11:35 +0300

    Update Carte.java

--- src/OOP/Principii/Polimorfism/Carte.java
+++ src/OOP/Principii/Polimorfism/Carte.java
@@ -3,14 +3,17 @@ package OOP.Principii.Polimorfism;
 public class Carte {
     String titlu;
     String autor;
+
     Carte(String titlu, String autor) {
         this.titlu = titlu;
         this.autor = autor;
     }
+
     void fisaCarte() {
         System.out.println("Titlu: " + titlu);
         System.out.println("Autor: " + autor);
     }
+
     void fisaCarte(int id) {
         System.out.println("Id: " + id);
         System.out.println("Titlu: " + titlu);
