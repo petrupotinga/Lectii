bffbf359f72cfb3308e46caf3d9f5c5872ff0f72
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-04-21 20:34:12 +0300

    getMessage()

---
+++ src/Exceptii/GFG1.java
@@ -0,0 +1,13 @@
+package Exceptii;
+
+public class GFG1 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 0;
+        try {
+            System.out.println(a / b);
+        } catch (ArithmeticException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
