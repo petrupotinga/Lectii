0341366a0dc6e43abb3eb4a14df19819e31812bd
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-04-26 17:44:00 +0300

    Update ExceptionThrow2.java

--- src/Exceptii/ExceptionThrow2.java
+++ src/Exceptii/ExceptionThrow2.java
@@ -1,18 +1,15 @@
 package Exceptii;
 
-import java.io.IOException;
 import java.util.Scanner;
 
 public class ExceptionThrow2 {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws ScannerException {
         Scanner scanner = new Scanner(System.in);
-        while(true){
+        while (true) {
             int x = Integer.parseInt(scanner.nextLine());
-            if(x != 1){
-                throw new IOException();
+            if (x < 10) {
+                throw new ScannerException("Utilizatorul ai introdus date incorecte!");
             }
-
         }
-
     }
 }
