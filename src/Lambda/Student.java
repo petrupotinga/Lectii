334b3f77cb50dd730d8354ff06280fe5769ab894
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-04-28 18:52:19 +0300

    Create Student.java

---
+++ src/Lambda/Student.java
@@ -0,0 +1,33 @@
+package Lambda;
+
+public class Student {
+    String name;
+    char sex;
+    int age;
+    int course;
+    double avgGrade;
+
+//    SELECT * FROM students WHERE sex = m AND avg > 5;
+
+
+    public Student(String name, char sex, int age, int course, double avgGrade) {
+        this.name = name;
+        this.sex = sex;
+        this.age = age;
+        this.course = course;
+        this.avgGrade = avgGrade;
+
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", sex=" + sex +
+                ", age=" + age +
+                ", course=" + course +
+                ", avgGrade=" + avgGrade +
+                '}';
+    }
+}
+
