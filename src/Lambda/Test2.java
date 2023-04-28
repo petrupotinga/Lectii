4b48f3702de0c2d750ed50bf58848e5d7368f8a1
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-04-28 18:52:26 +0300

    Create Test2.java

---
+++ src/Lambda/Test2.java
@@ -0,0 +1,14 @@
+package Lambda;
+
+public class Test2 {
+    static void def(I i){
+        System.out.println(i.abc("salut"));
+    }
+
+    public static void main(String[] args) {
+        def((String st)-> {return st.length();});
+    }
+}
+ interface I {
+    int abc(String s);
+ }
