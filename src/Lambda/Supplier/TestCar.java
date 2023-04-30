5cf4195a867a045dbb9f8788c1a7e380cb550235
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-04-30 13:28:07 +0300

    Create TestCar.java

---
+++ src/Lambda/Supplier/TestCar.java
@@ -0,0 +1,43 @@
+package Lambda.Supplier;
+
+import java.util.ArrayList;
+import java.util.function.Supplier;
+
+public class TestCar {
+    public static ArrayList<Car> createThreeCars(Supplier<Car> carSupplier){
+        ArrayList<Car> al = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            al.add(carSupplier.get());
+        }
+        return al;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Car> ourCars = createThreeCars(()->
+                new Car("Nissan Tiida",
+                "Silver metalic",
+                1.6));
+        System.out.println("Our cars: " + ourCars);
+
+    }
+}
+class Car{
+String model;
+String color;
+double engine;
+
+    public Car(String model, String color, double engine) {
+        this.model = model;
+        this.color = color;
+        this.engine = engine;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "model='" + model + '\'' +
+                ", color='" + color + '\'' +
+                ", engine=" + engine +
+                '}';
+    }
+}
