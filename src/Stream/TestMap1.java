fdd4d4999783bc0bb9721d801e70419eba1af97b
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-05-07 22:57:26 +0300

    Update TestMap1.java

--- src/Stream/TestMap1.java
+++ src/Stream/TestMap1.java
@@ -14,13 +14,15 @@ public class TestMap1 {
 //        for (int i = 0; i < list.size(); i++) {
 //            list.set(i, String.valueOf(list.get(i).length()));
 //        }
-     List<Integer> list2 =list.stream().map(element->
-             element.length()).collect(Collectors.toList());
+        List<Integer> list2 = list.stream().map(element ->
+                element.length()).collect(Collectors.toList());
 //        System.out.println(list);
-        int[]array = {5,9,3,8,1};
-        Arrays.stream(array).map(e->{ if(e%3==0){
-        e = e/3;
-        } return e;
+        int[] array = {5, 9, 3, 8, 1};
+        Arrays.stream(array).map(e -> {
+            if (e % 3 == 0) {
+                e = e / 3;
+            }
+            return e;
         }).toArray();
 //        System.out.println(Arrays.toString(array));
         Set<String> set = new TreeSet<>();
@@ -29,9 +31,9 @@ public class TestMap1 {
         set.add("ok");
         set.add("poka");
         System.out.println(set);
-        Set<Integer> set2 = set.stream().map(e->e.length()).collect(Collectors.toSet());
-        Set<Integer> set2 = set.stream().map(e->e.length()).collect(Collectors.toSet());
-
+        Set<Integer> set2 = set.stream().map(e -> e.length()).collect(Collectors.toSet());
+        List<Integer> list3 = set.stream().map(e -> e.length()).collect(Collectors.toList());
         System.out.println(set2);
+        System.out.println(list3);
     }
 }
