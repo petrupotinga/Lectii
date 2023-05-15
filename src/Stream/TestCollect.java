df24c1195b7c666acc5c8f1dcde5d17f5a1a69a7
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-05-15 14:26:38 +0300

    Create TestCollect.java

---
+++ src/Stream/TestCollect.java
@@ -0,0 +1,30 @@
+package Stream;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class TestCollect {
+    public static void main(String[] args) {
+        Student st1 = new Student("Ivan", 'm', 22, 3, 8.3);
+        Student st2 = new Student("Nicolai", 'm', 28, 2, 6.4);
+        Student st3 = new Student("Elena", 'f', 19, 1, 5.5);
+        Student st4 = new Student("Petru", 'm', 35, 4, 7);
+        Student st5 = new Student("Maria", 'f', 23, 3, 9.1);
+        List<Student> students = new ArrayList<>();
+        students.add(st1);
+        students.add(st2);
+        students.add(st3);
+        students.add(st4);
+        students.add(st5);
+
+        Map<Integer, List<Student>> map = students.stream().map(el -> {
+            el.setName(el.getName().toUpperCase());
+            return el;
+        }).collect(Collectors.groupingBy(Student::getCourse));
+        for (Map.Entry<Integer, List<Student>> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue().toString());
+        }
+    }
+}
