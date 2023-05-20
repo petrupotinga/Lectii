c302d8f8271d4d48908fb1739fe25be1967b550e
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-05-20 23:48:25 +0300

    Update TestMinMax.java

--- src/Stream/TestMinMax.java
+++ src/Stream/TestMinMax.java
@@ -2,6 +2,7 @@ package Stream;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class TestMinMax {
     public static void main(String[] args) {
@@ -17,16 +18,32 @@ public class TestMinMax {
         students.add(st4);
         students.add(st5);
 
+        List<Integer> courses = students.stream().mapToInt(Student::getCourse).boxed().collect(Collectors.toList());
+        System.out.println(courses);
+        int sum = students.stream().mapToInt(Student::getCourse).sum();
+        System.out.println(sum);
+        double average = students.stream().mapToInt(Student::getCourse).average().getAsDouble();
+        System.out.println(average);
+        int min = students.stream().mapToInt(Student::getCourse).min().getAsInt();
+        System.out.println(min);
+        int max = students.stream().mapToInt(Student::getCourse).max().getAsInt();
+        System.out.println(max);
+
+
+
+
+
+
 //        Student min = students.stream().min((x, y) -> x.getAge() - y.getAge()).get();
 //        System.out.println(min);
 //        Student max = students.stream().max((x, y) -> x.getAge() - y.getAge()).get();
 //        System.out.println(max);
 
-        students.stream().filter(e->e.getAge()>20).forEach(System.out::println);
-        System.out.println("-------------------");
-        students.stream().filter(e->e.getAge()>20).limit(2).forEach(System.out::println);
-        System.out.println("-------------------");
-        students.stream().filter(e->e.getAge()>20).skip(2).forEach(System.out::println);
+//        students.stream().filter(e->e.getAge()>20).forEach(System.out::println);
+//        System.out.println("-------------------");
+//        students.stream().filter(e->e.getAge()>20).limit(2).forEach(System.out::println);
+//        System.out.println("-------------------");
+//        students.stream().filter(e->e.getAge()>20).skip(2).forEach(System.out::println);
 
     }
 }
