19b9d87e73d5f404dead027af7fad1f6119ab5d3
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-07-18 13:13:16 +0300

    Update UserUpdate.java

--- src/JDBC/UserUpdate.java
+++ src/JDBC/UserUpdate.java
@@ -5,24 +5,24 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 public class UserUpdate {
     public static void main(String[] args) {
-        String url = "jdbc:mysql://localhost:3306/mydatabase";
-        String username = "myusername";
-        String password = "mypassword";
+        String url = "jdbc:postgresql://localhost:5432/NewDB";
+        String username = "postgres";
+        String password = "2015";
 
-        String newPassword = "newpassword";
-        String usernameToUpdate = "usernameToUpdate";
+        int newpersonid = 1;
+        String newname = "ION";
 
         try {
             // Pasul 1: Conectarea la baza de date
             Connection connection = DriverManager.getConnection(url, username, password);
 
             // Pasul 2: Construirea interogării UPDATE
-            String query = "UPDATE users SET password = ? WHERE username = ?";
+            String query = "UPDATE person SET name = ? WHERE personid = ?";
             PreparedStatement statement = connection.prepareStatement(query);
 
             // Pasul 3: Setarea parametrilor
-            statement.setString(1, newPassword);
-            statement.setString(2, usernameToUpdate);
+            statement.setString(1, newname);
+            statement.setInt(2, newpersonid);
 
             // Pasul 4: Executarea interogării UPDATE
             int rowsAffected = statement.executeUpdate();
